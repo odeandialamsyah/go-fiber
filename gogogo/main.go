bfec7651d2bf2b8dd4288352b5942be3a10a63d0
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-07 19:39:24 +0700

    create main.go for try to make hello world

---
+++ gogogo/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Print("hello world")
+
+	app := fiber.New()
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
