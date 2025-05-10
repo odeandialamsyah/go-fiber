3db402c3dcad4bcd35e6b0a6cd74c445a1493b55
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-10 20:56:10 +0700

    create func HashPassword in Utils

---
+++ basic-authentication-with-go-fiber/utils/hash.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword meng-hash password pengguna
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
