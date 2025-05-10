3a0e1e74090569a8c2c2f376977ddc34604800ca
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-10 20:52:37 +0700

    Create Model Struct User

---
+++ basic-authentication-with-go-fiber/models/User.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Username string             `json:"username" bson:"username"`
+	Email    string             `json:"email" bson:"email"`
+	Password string             `json:"password" bson:"password"`
+	RoleID   primitive.ObjectID `json:"role_id" bson:"role_id"`
+	Role     Role               `json:"role" bson:"role"`
+}
+
