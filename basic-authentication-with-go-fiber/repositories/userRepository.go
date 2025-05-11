1b673471b136cff721757dd1999bd2567963b7d6
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-11 23:36:47 +0700

    create func GetUserByEmail in repositories

--- basic-authentication-with-go-fiber/repositories/userRepository.go
+++ basic-authentication-with-go-fiber/repositories/userRepository.go
@@ -5,11 +5,22 @@ import (
 	"basic-authentication-with-go-fiber/models"
 	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var userCollection *mongo.Collection = config.DB.Collection("users")
 var roleCollection *mongo.Collection = config.DB.Collection("roles")
+
+// Fungsi untuk mendapatkan user berdasarkan email
+func GetUserByEmail(email string) (models.User, error) {
+	var user models.User
+	err := userCollection.FindOne(context.Background(), bson.M{"email": email}).Decode(&user)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
 
 // Fungsi untuk menambahkan user baru
 func CreateUser(user models.User) error {
