ac62d730976f30b50ab34ffcdc5483148935586b
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-11 23:34:15 +0700

    create func createUser in repositories

--- basic-authentication-with-go-fiber/repositories/userRepository.go
+++ basic-authentication-with-go-fiber/repositories/userRepository.go
@@ -2,9 +2,20 @@ package repositories
 
 import (
 	"basic-authentication-with-go-fiber/config"
+	"basic-authentication-with-go-fiber/models"
+	"context"
 
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var userCollection *mongo.Collection = config.DB.Collection("users")
 var roleCollection *mongo.Collection = config.DB.Collection("roles")
+
+// Fungsi untuk menambahkan user baru
+func CreateUser(user models.User) error {
+	_, err := userCollection.InsertOne(context.Background(), user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
