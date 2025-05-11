d17b40ee27d32b113b53765c14ba8dcb8491a889
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-11 23:23:59 +0700

    create func roleMiddleware for check role

--- basic-authentication-with-go-fiber/middleware/authMiddleware.go
+++ basic-authentication-with-go-fiber/middleware/authMiddleware.go
@@ -1,11 +1,15 @@
 package middleware
 
 import (
+	"basic-authentication-with-go-fiber/config"
 	"basic-authentication-with-go-fiber/models"
-	"go-mvc-auth/utils"
+	"basic-authentication-with-go-fiber/utils"
 	"strings"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // AuthMiddleware memverifikasi JWT token di header Authorization
@@ -23,4 +27,50 @@ func AuthMiddleware(c *fiber.Ctx) error {
 
 	c.Locals("userID", claims["sub"])
 	return c.Next()
+}
+
+// RoleMiddleware memverifikasi apakah pengguna memiliki role yang diperlukan
+func RoleMiddleware(requiredRole string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// Ambil userID yang disimpan di context oleh middleware Auth
+		userID := c.Locals("userID")
+		if userID == nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+
+		// Pastikan userID dikonversi ke ObjectID
+		objID, ok := userID.(primitive.ObjectID)
+		if !ok {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Invalid user ID format",
+			})
+		}
+
+		// Ambil user dari database menggunakan ObjectID
+		userCollection := config.DB.Collection("users")
+		var user models.User
+		err := userCollection.FindOne(c.Context(), bson.M{"_id": objID}).Decode(&user)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"error": "User not found",
+				})
+			}
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Internal server error",
+			})
+		}
+
+		// Cek apakah role pengguna sesuai dengan role yang diminta
+		if user.Role.Name != requiredRole {
+			return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+				"error": "Access Denied",
+			})
+		}
+
+		// Lanjutkan ke handler berikutnya
+		return c.Next()
+	}
 }
