9c089c177842cf0e1083616eaa494aa370103108
ODE ANDI ALAMSYAH <147012411+odeandialamsyah@users.noreply.github.com>
2025-05-14 23:34:53 +0700

    main.go

--- basic-authentication-with-go-fiber/main.go
+++ basic-authentication-with-go-fiber/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"basic-authentication-with-go-fiber/config"
+	"basic-authentication-with-go-fiber/routes"
+	"basic-authentication-with-go-fiber/utils"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
-	
+	// Menghubungkan ke MongoDB
+	config.ConnectDB()
+
+	// Seed role default jika belum ada
+	utils.SeedRole(config.DB)
+
+	// Membuat aplikasi Fiber
+	app := fiber.New()
+
+	// Menambahkan routes
+	routes.UserRoutes(app)
+
+	// Menjalankan server di port 3000
+	log.Fatal(app.Listen(":3000"))
 }
