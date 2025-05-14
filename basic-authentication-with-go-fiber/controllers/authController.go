10406a9cfc60fc94cb1a04540297dee57090dd8a
ODE ANDI ALAMSYAH <147012411+odeandialamsyah@users.noreply.github.com>
2025-05-14 23:06:26 +0700

    update controller

--- basic-authentication-with-go-fiber/controllers/authController.go
+++ basic-authentication-with-go-fiber/controllers/authController.go
@@ -67,8 +67,3 @@ func LoginUser(c *fiber.Ctx) error {
 		"token": token,
 	})
 }
-
-// Fungsi dashboard admin (contoh protected route)
-func AdminDashboard(c *fiber.Ctx) error {
-	return c.SendString("Welcome to the admin dashboard!")
-}
