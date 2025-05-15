dba28d81bd11022a90f7ca1316ff73ee91ea2096
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-15 23:19:30 +0700

    reconfig

---
+++ basic-authentication-with-go-fiber/config/database.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB() {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = client.Database("auth_system")
+	log.Println("Connected to MongoDB!")
+}
