5b224328bd8c9c128e6514cff3f9e87244828411
odeandialamsyah <odeandialamsyah@gmail.com>
2025-05-15 23:26:37 +0700

    update route

--- basic-authentication-with-go-fiber/routes/route.go
+++ basic-authentication-with-go-fiber/routes/route.go
@@ -1,6 +1,6 @@
 package routes
 
-import(
+import (
 	"basic-authentication-with-go-fiber/controllers"
 	"basic-authentication-with-go-fiber/middleware"
 
@@ -10,7 +10,31 @@ import(
 func UserRoutes(app *fiber.App) {
 	api := app.Group("/api")
 
-	// Register routes
+	// Public routes
 	api.Post("/register", controllers.RegisterUser)
 	api.Post("/login", controllers.LoginUser)
+
+	// Protected routes
+	protected := api.Group("/protected")
+	protected.Use(middleware.AuthMiddleware)
+
+	// Admin routes
+	admin := protected.Group("/admin")
+	admin.Use(middleware.RoleMiddleware("admin"))
+	// Add admin endpoints
+	admin.Get("/stats", controllers.GetAdminStats)
+	admin.Get("/users", controllers.GetAllUsers)
+	admin.Get("/users/:id", controllers.GetUser)
+	admin.Put("/users/:id", controllers.UpdateUser)
+	admin.Delete("/users/:id", controllers.DeleteUser)
+	admin.Get("/roles", controllers.GetAllRoles)
+	admin.Delete("/roles/:id", controllers.DeleteRole)
+
+	// User routes
+	user := protected.Group("/user")
+	user.Use(middleware.RoleMiddleware("user"))
+	// Add user endpoints
+	user.Get("/profile", controllers.GetUserProfile)
+	user.Put("/profile", controllers.UpdateUserProfile)
+	user.Post("/change-password", controllers.ChangePassword)
 }
